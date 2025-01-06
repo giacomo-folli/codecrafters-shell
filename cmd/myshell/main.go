77bd30b45dd1f2c12c3df44917a1207eba7d2ada
Giacomo <giacomo.folli@studenti.unipr.it>
2025-01-06 10:38:43 +0100

    output error

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
 func main() {
@@ -14,5 +13,15 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "Error in reading string")
+		os.Exit(1)
+	}
+
+	fmt.Println(command[:len(command)-1], ": command not found ")
+	// _, err = exec.LookPath(userInput)
+	//if err != nil {
+	//	fmt.Printf("%s: Command not found", userInput)
+	//}
 }
