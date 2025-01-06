499f0e1c98fc45f018bd19877143f5a610df9932
Giacomo <giacomo.folli@studenti.unipr.it>
2025-01-06 11:03:46 +0100

    add REPL env

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,19 +9,21 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		// Uncomment this block to pass the first stage
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintf(os.Stdout, "Error in reading string")
-		os.Exit(1)
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "Error in reading string")
+			os.Exit(1)
+		}
+
+		fmt.Println(command[:len(command)-1] + ": command not found ")
+		// _, err = exec.LookPath(userInput)
+		//if err != nil {
+		//	fmt.Printf("%s: Command not found", userInput)
+		//}
 	}
-
-	fmt.Println(command[:len(command)-1] + ": command not found ")
-	// _, err = exec.LookPath(userInput)
-	//if err != nil {
-	//	fmt.Printf("%s: Command not found", userInput)
-	//}
 }
