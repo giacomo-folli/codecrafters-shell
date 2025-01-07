244b3230d36452aa680fdc0ae79ec1dc55e621f9
Giacomo <giacomo.folli@studenti.unipr.it>
2025-01-07 17:29:43 +0100

    add exit command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,26 +4,27 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
-
-var _ = fmt.Fprint
 
 func main() {
 	for {
-		// Uncomment this block to pass the first stage
 		fmt.Fprint(os.Stdout, "$ ")
 
 		// Wait for user input
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		userInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprintf(os.Stdout, "Error in reading string")
 			os.Exit(1)
 		}
 
-		fmt.Println(command[:len(command)-1] + ": command not found ")
-		// _, err = exec.LookPath(userInput)
-		//if err != nil {
-		//	fmt.Printf("%s: Command not found", userInput)
-		//}
+		command := strings.TrimRight(userInput, "\r\n")
+
+		if command == "exit 0" {
+			os.Exit(0)
+		} else {
+			// Print the command not found message
+			fmt.Println(command + ": command not found")
+		}
 	}
 }
